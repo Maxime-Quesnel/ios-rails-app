b2fc6d6d3d4c88b721cdf0646d6489fc0f9723c6
Maximeqnl <maxime@devbots.io>
2022-06-29 14:38:02 +0200

    Add navigation

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,22 @@
+class TurboFailureApp < Devise::FailureApp
+  include Turbo::Native::Navigation
+
+  def respond
+    if request_format == :turbo_stream
+      redirect
+    elsif turbo_native_app?
+      http_auth
+    else
+      super
+    end
+  end
+
+  def skip_format?
+    %w[html turbo_stream */*].include? request_format.to_s
+  end
+end
+
+
 # frozen_string_literal: true
 
 # Assuming you have not yet modified this file, each configuration option below
